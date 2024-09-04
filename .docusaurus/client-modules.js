daf67c396678e0156e64abb91ab5f70e12b40081
Slynyr <99285452+Slynyr@users.noreply.github.com>
2024-09-03 22:41:33 -0400

    Deploy website - based on 63109c2a5ff7e07cd43bd3fb254f7f939f656cb8

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\Users\\filip\\Documents\\GitHub\\Atlas\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\filip\\Documents\\GitHub\\Atlas\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\filip\\Documents\\GitHub\\Atlas\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\filip\\Documents\\GitHub\\Atlas\\src\\css\\custom.css"),
-];
